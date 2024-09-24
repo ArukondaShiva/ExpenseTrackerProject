2523539a78f7c9c36805794374e5bfb6e3dcbcca
shiva <a.shiva16052000@gmail.com>
2024-09-24 08:13:20 +0530

    Expense Tracker - Added Validations.

--- expensetrackerapi/src/main/java/com/example/expensetracker/entity/Expense.java
+++ expensetrackerapi/src/main/java/com/example/expensetracker/entity/Expense.java
@@ -1,6 +1,9 @@
 package com.example.expensetracker.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,25 +22,38 @@ import java.sql.Timestamp;
 @Table(name = "tbl_expenses")
 public class Expense {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(name = "expense_name")
+    @NotBlank(message = "Expense name must not be null")
+    @Size(min = 3,message = "Expense name should have atleast 3 characters")
     private String name;
+
 
     private String description;
 
+
     @Column(name = "expense_amount")
+    @NotNull(message = "expense amount must not be null")
     private BigDecimal amount;
 
+
+    @NotBlank(message = "Category must not be null")
     private String category;
 
+
+    @NotNull(message = "Date must not be null")
     private Date date;
+
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
     private Timestamp createdAt;
+
 
     @Column(name = "updated_at")
     @UpdateTimestamp
--- expensetrackerapi/src/main/java/com/example/expensetracker/exceptions/GlobalExceptionHandler.java
+++ expensetrackerapi/src/main/java/com/example/expensetracker/exceptions/GlobalExceptionHandler.java
@@ -2,17 +2,30 @@ package com.example.expensetracker.exceptions;
 
 
 import com.example.expensetracker.entity.ErrorObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+// GlobalExceptionHandler extending ResponseEntityExceptionHandler to customize the error response.
+
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorObject> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
@@ -54,4 +67,25 @@ public class GlobalExceptionHandler {
     }
 
 
+    // This is the method which will helps to customize the error response.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        //return super.handleMethodArgumentNotValid(ex, headers, status, request);
+
+        Map<String,Object> body = new HashMap<String,Object>();
+
+        body.put("timestamp",new Date());
+        body.put("statusCode",HttpStatus.BAD_REQUEST.value());
+
+        List<String> errors = ex.getBindingResult()
+                        .getFieldErrors()
+                                .stream()
+                                        .map(x->x.getDefaultMessage())
+                                                .collect(Collectors.toList());
+
+        body.put("messages",errors);
+
+        return new ResponseEntity<Object>(body,HttpStatus.BAD_REQUEST);
+    }
 }
